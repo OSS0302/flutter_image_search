dab154cef9321ce6b12b65a9d9573f3ffe072952
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-28 18:38:11 +0900

    03 mvvm + provider + getIt

--- lib/presentation/pixabay/pixabay_screen.dart
+++ lib/presentation/pixabay/pixabay_screen.dart
@@ -2,6 +2,7 @@ import 'package:flutter/material.dart';
 import 'package:image_search_app/data/repository/pixabay_repository_impl.dart';
 import 'package:image_search_app/presentation/pixabay/pixabay_view_model.dart';
 import 'package:image_search_app/presentation/widget/pixabay_widget.dart';
+import 'package:provider/provider.dart';
 
 class PixabayScreen extends StatefulWidget {
   const PixabayScreen({super.key});
@@ -12,7 +13,6 @@ class PixabayScreen extends StatefulWidget {
 
 class _PixabayScreenState extends State<PixabayScreen> {
   final textEditingController = TextEditingController();
-  final pixabayViewModel = PixabayViewModel();
 
   @override
   void dispose() {
@@ -22,6 +22,7 @@ class _PixabayScreenState extends State<PixabayScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final pixabayViewModel = context.read<PixabayViewModel>();
     return Scaffold(
       appBar: AppBar(
         title: const Text('pixabay image app'),
@@ -64,33 +65,29 @@ class _PixabayScreenState extends State<PixabayScreen> {
               SizedBox(
                 height: 24,
               ),
-              StreamBuilder<bool>(
-                initialData: false,
-                  stream: pixabayViewModel.isLoadingStream, builder: (context, snapshot){
-                if(snapshot.data! == true) {
-                  return Center(
-                    child: Column(children: [
-                      CircularProgressIndicator(),
-                      Text('잠시만 기다려 주세요'),
-                    ],),
-                  );
-                }
-                return  Expanded(
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        mainAxisSpacing: 32,
-                        crossAxisSpacing: 32),
-                    itemCount: pixabayViewModel.pixabayItem.length,
-                    itemBuilder: (context, index) {
-                      final pixabayItems =
-                      pixabayViewModel.pixabayItem[index];
-                      return PixabayWidget(pixabayItem: pixabayItems);
-                    },
-                  ),
-                );
-              }),
-
+              pixabayViewModel.isLoading
+                  ? Center(
+                      child: Column(
+                        children: [
+                          CircularProgressIndicator(),
+                          Text('잠시만 기다려 주세요'),
+                        ],
+                      ),
+                    )
+                  : Expanded(
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            mainAxisSpacing: 32,
+                            crossAxisSpacing: 32),
+                        itemCount: pixabayViewModel.pixabayItem.length,
+                        itemBuilder: (context, index) {
+                          final pixabayItems =
+                              pixabayViewModel.pixabayItem[index];
+                          return PixabayWidget(pixabayItem: pixabayItems);
+                        },
+                      ),
+                    ),
             ],
           ),
         ),
