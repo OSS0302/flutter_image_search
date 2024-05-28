105e1e2665d1d11a65b7760900a26892569deb2d
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-28 21:04:44 +0900

    09 Clean Architecture + provider + getIt + router + state + result + freezed + showSnackBar + ShowDialog

--- lib/presentation/pixabay/pixabay_view_model.dart
+++ lib/presentation/pixabay/pixabay_view_model.dart
@@ -2,17 +2,19 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:image_search_app/core/result.dart';
+import 'package:image_search_app/domain/use_case/search_use_case.dart';
 import 'package:image_search_app/presentation/pixabay/pixabay_event.dart';
 import 'package:image_search_app/presentation/pixabay/pixabay_state.dart';
 
-import '../../data/model/pixabay_item.dart';
-import '../../data/repository/pixabay_repository.dart';
+import '../../domain/model/pixabay_item.dart';
+import '../../domain/repository/pixabay_repository.dart';
 
 class PixabayViewModel extends ChangeNotifier {
-  final PixabayRepository _repository ;
-   PixabayViewModel({
-    required PixabayRepository repository,
-  }) : _repository = repository;
+ final SearchUseCase _searchUseCase;
+
+ PixabayViewModel({
+   required SearchUseCase searchUseCase,
+ }) : _searchUseCase = searchUseCase;
 
   PixabayState _state =  PixabayState(isLoading: false, pixabayItem: List.unmodifiable([]));
 
@@ -29,7 +31,7 @@ class PixabayViewModel extends ChangeNotifier {
     );
     notifyListeners();
 
-    final result = await _repository.getPixabayItem(query);
+    final result = await _searchUseCase.execute(query);
     switch(result) {
 
       case Success<List<PixabayItem>>():
@@ -47,6 +49,4 @@ class PixabayViewModel extends ChangeNotifier {
     }
     notifyListeners();
   }
-
-
 }
