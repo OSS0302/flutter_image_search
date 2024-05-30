08c8362475f6653abd0faeb3f69822b1071933ee
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-05-30 17:19:21 +0900

    09 Clean Architecture + provider + getIt + router + state + result + freezed + showSnackBar + ShowDialog

--- lib/data/repository/pixabay_repository_impl.dart
+++ lib/data/repository/pixabay_repository_impl.dart
@@ -1,9 +1,9 @@
 import 'package:image_search_app/data/data_source/pixabay_api.dart';
 import 'package:image_search_app/data/mapper/pixabay_mapper.dart';
-import 'package:image_search_app/data/model/pixabay_item.dart';
-import 'package:image_search_app/data/repository/pixabay_repository.dart';
+import 'package:image_search_app/domain/repository/pixabay_repository.dart';
 
 import '../../core/result.dart';
+import '../../domain/model/pixabay_item.dart';
 
 class PixabayRepositoryImpl implements PixabayRepository{
 
@@ -14,7 +14,7 @@ class PixabayRepositoryImpl implements PixabayRepository{
     try {
       final dto = await _api.getImageResult(query);
       if(dto.hits == null) {
-        return Result.success([]);
+        return const Result.success([]);
       }
       return Result.success(dto.hits!.map((e) => e.toPixabayItem()).toList());
     }catch(e) {
