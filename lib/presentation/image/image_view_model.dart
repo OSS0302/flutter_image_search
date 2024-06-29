142927f60da9f0d2aee39fb3b661c15306d2005a
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-06-29 15:48:36 +0900

    09 cleanArchitecture + provider + getIt + router + state + result + freezed + showSnackBar + showDialog

--- lib/presentation/image/image_view_model.dart
+++ lib/presentation/image/image_view_model.dart
@@ -2,18 +2,18 @@ import 'dart:async';
 
 import 'package:flutter/material.dart';
 import 'package:image_search_app/core/result.dart';
+import 'package:image_search_app/domain/use_case/search_use_case.dart';
 import 'package:image_search_app/presentation/image/image_event.dart';
 
-import '../../data/model/image_item.dart';
-import '../../data/repository/image_repository.dart';
+import '../../domain/model/image_item.dart';
 import 'image_state.dart';
 
 class ImageViewModel extends ChangeNotifier {
-  final ImageRepository _repository;
+  final SearchUseCase  _searchUseCase;
 
-  ImageViewModel({
-    required ImageRepository repository,
-  }) : _repository = repository;
+   ImageViewModel({
+    required SearchUseCase searchUseCase,
+  }) : _searchUseCase = searchUseCase;
 
   ImageState _state = ImageState(
     imageItem: List.unmodifiable([]),
@@ -32,7 +32,7 @@ class ImageViewModel extends ChangeNotifier {
     );
     notifyListeners();
 
-    final result = await _repository.getImageItems(query);
+    final result = await _searchUseCase.execute(query);
     switch(result){
 
       case Success<List<ImageItem>>():
