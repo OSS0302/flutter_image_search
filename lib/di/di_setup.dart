4a5192590b3f7876b16689ece8a3960f62d28e50
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-06-30 18:00:17 +0900

    10 cleanArchitecture + provider + getIt + router + state + result + freezed + showSnackBar + showDialog + hero

--- lib/di/di_setup.dart
+++ lib/di/di_setup.dart
@@ -1,8 +1,9 @@
 import 'package:get_it/get_it.dart';
-import 'package:image_search_app/domain/repository/image_repository.dart';
 import 'package:image_search_app/data/repository/image_repository_impl.dart';
 import 'package:image_search_app/domain/use_case/search_use_case.dart';
 import 'package:image_search_app/presentation/image/image_view_model.dart';
+
+import '../domain/repository/image_repository.dart';
 
 final getIt = GetIt.instance;
 
