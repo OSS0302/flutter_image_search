dfa879a4a5e693818c6516935cff59a96e4dd392
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-07-01 16:36:46 +0900

    00 FutureBuilder

--- lib/data/data_source/pixabay_api.dart
+++ lib/data/data_source/pixabay_api.dart
@@ -1,11 +1,11 @@
 import 'dart:convert';
 
 import '../../key.dart';
-import '../dto/image_dto.dart';
+import '../dto/pixabay_dto.dart';
 import 'package:http/http.dart' as http;
-class ImageApi {
-  Future<ImageDto> getImageResult(String query) async{
+class PixabayAPi {
+  Future<PixabayDto> getImageResult(String query) async{
     final response = await http.get(Uri.parse('https://pixabay.com/api/?key=$key&q=$query&image_type=photo&pretty=true'));
-    return ImageDto.fromJson(jsonDecode(response.body));
+    return PixabayDto.fromJson(jsonDecode(response.body));
   }
 }
---
+++ lib/ui/pixabay/pixabay_screen.dart
@@ -0,0 +1,101 @@
+import 'package:flutter/material.dart';
+import 'package:image_search_app/data/model/pixabay_item.dart';
+import 'package:image_search_app/data/repository/pixabay_repository_impl.dart';
+import 'package:image_search_app/ui/widget/pixabay_widget.dart';
+
+class PixabayScreen extends StatefulWidget {
+  const PixabayScreen({super.key});
+
+  @override
+  State<PixabayScreen> createState() => _PixabayScreenState();
+}
+
+class _PixabayScreenState extends State<PixabayScreen> {
+  final pixabaySearchController = TextEditingController();
+
+  @override
+  void dispose() {
+    pixabaySearchController.dispose();
+    super.dispose();
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text('이미지 검색앱'),
+      ),
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: pixabaySearchController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: Colors.tealAccent,
+                      width: 2,
+                    ),
+                  ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: Colors.tealAccent,
+                      width: 2,
+                    ),
+                  ),
+                  hintText: '이미지를 검색 하세요',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.search_rounded,
+                      color: Colors.tealAccent,
+                    ),
+                    onPressed: () {
+                      setState(() {});
+                    },
+                  ),
+                ),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              FutureBuilder<List<PixabayItem>>(
+                future: PixabayRepositoryImpl()
+                    .getPixabayItem(pixabaySearchController.text),
+                builder: (context, snapshot) {
+                  if (!snapshot.hasData) {
+                    return Center(
+                      child: Column(
+                        children: [
+                          CircularProgressIndicator(),
+                          Text('잠시만 기달려 주세요'),
+                        ],
+                      ),
+                    );
+                  }
+                  final pixabayItem = snapshot.data!;
+                  return Expanded(
+                    child: GridView.builder(
+                      gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                          crossAxisCount: 4,
+                          mainAxisSpacing: 32,
+                          crossAxisSpacing: 32),
+                      itemCount: pixabayItem.length,
+                      itemBuilder: (context, index) {
+                        final pixabayItems = pixabayItem[index];
+                        return PixabayWidget(pixabayItems: pixabayItems);
+                      },
+                    ),
+                  );
+                },
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
+  }
+}
