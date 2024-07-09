0746a0ad55630f2d3c6a5057ade84635acb67691
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-07-09 15:39:19 +0900

    06 mvvm + provider + getIt + router + state + result

--- lib/presentation/image/image_screen.dart
+++ lib/presentation/image/image_screen.dart
@@ -57,8 +57,14 @@ class _ImageScreenState extends State<ImageScreen> {
                       color: Colors.redAccent,
                     ),
                     onPressed: () async {
-                      await imageViewModel
+                     final result =  await imageViewModel
                           .fetchImage(imageSearchController.text);
+                     if(result == false) {
+                       const snackBar = SnackBar(content: Text('네트워크 오류'));
+                       if(mounted){
+                         ScaffoldMessenger.of(context).showSnackBar(snackBar);
+                       }
+                     }
                       setState(() {});
                     },
                   ),
