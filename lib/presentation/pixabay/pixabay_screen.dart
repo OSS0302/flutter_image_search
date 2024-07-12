6b04ba57d124563d4f417ab5559208b942f744a8
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-07-12 15:49:16 +0900

    01 mvvm

--- lib/presentation/pixabay/pixabay_screen.dart
+++ lib/presentation/pixabay/pixabay_screen.dart
@@ -1,6 +1,7 @@
 import 'package:flutter/material.dart';
 import 'package:image_search_app/data/model/pixabay_item.dart';
 import 'package:image_search_app/data/repository/pixabay_repository_impl.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_view_model.dart';
 import 'package:image_search_app/presentation/widget/pixabay_widget.dart';
 
 class PixabayScreen extends StatefulWidget {
@@ -12,6 +13,7 @@ class PixabayScreen extends StatefulWidget {
 
 class _PixabayScreenState extends State<PixabayScreen> {
   final pixabaySearchController = TextEditingController();
+  final pixabayViewModel = PixabayViewModel();
 
   @override
   void dispose() {
@@ -26,66 +28,72 @@ class _PixabayScreenState extends State<PixabayScreen> {
         title: const Text('pixbay Search App'),
       ),
       body: SafeArea(
-          child: Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: Column(
-              children: [
-                TextField(
-                  controller: pixabaySearchController,
-                  decoration: InputDecoration(
-                    focusedBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(
-                        width: 2,
-                        color: Colors.blueAccent,
-                      ),
-                    ),
-                    enabledBorder: OutlineInputBorder(
-                      borderRadius: BorderRadius.circular(20),
-                      borderSide: BorderSide(
-                        width: 2,
-                        color: Colors.blueAccent,
-                      ),
-                    ),
-                    hintText: '이미지를 검색하세요',
-                    suffixIcon: IconButton(
-                      icon: Icon(
-                        Icons.ads_click_outlined,
-                        color: Colors.blueAccent,
-                      ),
-                      onPressed: () {
-                        setState(() {});
-                      },
+        child: Padding(
+          padding: const EdgeInsets.all(8.0),
+          child: Column(
+            children: [
+              TextField(
+                controller: pixabaySearchController,
+                decoration: InputDecoration(
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.blueAccent,
                     ),
                   ),
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      width: 2,
+                      color: Colors.blueAccent,
+                    ),
+                  ),
+                  hintText: '이미지를 검색하세요',
+                  suffixIcon: IconButton(
+                    icon: Icon(
+                      Icons.ads_click_outlined,
+                      color: Colors.blueAccent,
+                    ),
+                    onPressed: () async {
+                      await pixabayViewModel
+                          .searchImage(pixabaySearchController.text);
+
+                      setState(() {});
+                    },
+                  ),
                 ),
-                SizedBox(height: 24,),
-                FutureBuilder<List<PixabayItem>>(future: PixabayRepositoryImpl().getPixabayItem(
-                    pixabaySearchController.text),
-                    builder: (context, snapshot) {
-                      if (!snapshot.hasData) {
-                        return Center(child: Column(
-                          children: [
-                            CircularProgressIndicator(),
-                            Text('잠시만 기다려 주세요 데이터 로딩중입니다.'),
-                          ],
-                        ),);
-                      }
-                      final pixabayItem = snapshot.data!;
-                      return Expanded(child: GridView.builder(
-                          gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                              crossAxisCount: 4,
-                              crossAxisSpacing: 32,
-                              mainAxisSpacing: 32),
-                          itemCount: pixabayItem.length,
-                          itemBuilder: (context, index) {
-                              final pixabayItems = pixabayItem[index];
-                              return PixabayWidget(pixabayItems: pixabayItems);
-                          }));
-                    })
-              ],
-            ),
-          )),
+              ),
+              SizedBox(
+                height: 24,
+              ),
+              pixabayViewModel.isLoading
+                  ? Center(
+                      child: Column(
+                        children: [
+                          CircularProgressIndicator(),
+                          Text('잠시만 기다려 주세요 데이터 로딩중입니다.'),
+                        ],
+                      ),
+                    )
+                  : Expanded(
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            crossAxisSpacing: 32,
+                            mainAxisSpacing: 32),
+                        itemCount: pixabayViewModel.pixabayItem.length,
+                        itemBuilder: (context, index) {
+                          final pixabayItems =
+                              pixabayViewModel.pixabayItem[index];
+                          return PixabayWidget(pixabayItems: pixabayItems);
+                        },
+                      ),
+                    ),
+            ],
+          ),
+        ),
+      ),
     );
   }
 }
