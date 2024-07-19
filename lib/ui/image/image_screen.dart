8f9e67b14a15257ef135d9a2c06aded06208337a
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-07-19 15:24:02 +0900

    08 mvvm + provider + getIt + router + state + result + freezed + showSnackBar + showDialog

--- lib/ui/image/image_screen.dart
+++ lib/ui/image/image_screen.dart
@@ -1,9 +1,13 @@
+import 'dart:async';
+
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
 import 'package:image_search_app/ui/image/image_view_model.dart';
 import 'package:image_search_app/ui/widget/image_widget.dart';
 import 'package:provider/provider.dart';
 
 import '../../data/model/image_item.dart';
+import 'image_event.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -14,11 +18,49 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   final imageSearchController = TextEditingController();
+  StreamSubscription<ImageEvent>? subscription;
 
+  @override
+  void initState() {
+    Future.microtask(() {
+      subscription = context.read<ImageViewModel>().eventStream.listen((event) {
+        switch (event) {
+          case ShowSnackBar():
+            final snackBar = SnackBar(content: Text(event.message));
+            ScaffoldMessenger.of(context).showSnackBar(snackBar);
+          case ShowDialog():
+            showDialog(
+                context: context,
+                builder: (context) {
+                  return AlertDialog(
+                    title: Text('image Search App'),
+                    content: Text('이미지 가져오기 완료'),
+                    actions: [
+                      Container(
+                        decoration: BoxDecoration(
+                          borderRadius: BorderRadius.circular(20),
+                          color: Colors.blueAccent,
+
+                        ),
+                        child: TextButton(
+                            onPressed: () {
+                              context.pop();
+                            },
+                            child: Text('확인')),
+                      )
+                    ],
+                  );
+                });
+        }
+      });
+    });
+    super.initState();
+  }
 
   @override
   void dispose() {
-    imageSearchController.text;
+    subscription?.cancel();
+    imageSearchController.dispose();
     super.dispose();
   }
 
