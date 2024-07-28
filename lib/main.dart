366d4502787d2d6755879b81827cc4cc97a4985c
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-07-28 16:45:32 +0900

    03 mvvm + provider + getIt

--- lib/main.dart
+++ lib/main.dart
@@ -1,10 +1,15 @@
 import 'package:flutter/material.dart';
+import 'package:image_search_app/data/repository/image_repository_impl.dart';
+import 'package:image_search_app/di/di_setup.dart';
 import 'package:image_search_app/presentation/image/image_screen.dart';
+import 'package:image_search_app/presentation/image/image_view_model.dart';
 import 'package:image_search_app/routes.dart';
+import 'package:provider/provider.dart';
 
 
 
 void main() {
+  diSetUp();
   runApp(const MyApp());
 }
 
@@ -20,7 +25,10 @@ class MyApp extends StatelessWidget {
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
       ),
-       home: ImageScreen() ,
+       home: ChangeNotifierProvider(
+         create: (_) => ImageViewModel(repository: ImageRepositoryImpl()),
+         child: ImageScreen(),
+       ),
     );
   }
 }
--- lib/presentation/image/image_screen.dart
+++ lib/presentation/image/image_screen.dart
@@ -1,7 +1,7 @@
 import 'package:flutter/material.dart';
-import 'package:image_search_app/data/repository/image_repository_impl.dart';
 import 'package:image_search_app/presentation/image/image_view_model.dart';
 import 'package:image_search_app/presentation/widget/image_widget.dart';
+import 'package:provider/provider.dart';
 
 class ImageScreen extends StatefulWidget {
   const ImageScreen({super.key});
@@ -12,7 +12,7 @@ class ImageScreen extends StatefulWidget {
 
 class _ImageScreenState extends State<ImageScreen> {
   final imageSearchController = TextEditingController();
-  final imageViewModel = ImageViewModel();
+
 
   @override
   void dispose() {
@@ -22,6 +22,8 @@ class _ImageScreenState extends State<ImageScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final imageViewModel = context.read<ImageViewModel>();
+
     return Scaffold(
       appBar: AppBar(
         title: Text('Image Search App'),
@@ -65,33 +67,28 @@ class _ImageScreenState extends State<ImageScreen> {
               SizedBox(
                 height: 24,
               ),
-              StreamBuilder<bool>(
-                initialData: false,
-                  stream: imageViewModel.isLoadingStream, builder: (context, snapshot) {
-                if(snapshot.data! == true) {
-                  return Column(
-                    children: [
-                      CircularProgressIndicator(),
-                      Text('잠시만 기다려 주세요.'),
-                      Text('로딩 중 입니다.'),
-                    ],
-                  );
-                }
-                return Expanded(
-                  child: GridView.builder(
-                    gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
-                        crossAxisCount: 4,
-                        mainAxisSpacing: 32,
-                        crossAxisSpacing: 32),
-                    itemCount: imageViewModel.imageItem.length,
-                    itemBuilder: (context, index) {
-                      final imageItems = imageViewModel.imageItem[index];
-                      return ImageWidget(imageItems: imageItems);
-                    },
+              imageViewModel.isLoading ? Center(
+                child: Column(
+                  children: [
+                    CircularProgressIndicator(),
+                    Text('잠시만 기다려 주세요 '),
+                    Text('로딩 중 입니다.'),
+                  ],
+                ),
+              )
+              : Expanded(
+                      child: GridView.builder(
+                        gridDelegate: SliverGridDelegateWithFixedCrossAxisCount(
+                            crossAxisCount: 4,
+                            mainAxisSpacing: 32,
+                            crossAxisSpacing: 32),
+                        itemCount: imageViewModel.imageItem.length,
+                        itemBuilder: (context, index) {
+                          final imageItems = imageViewModel.imageItem[index];
+                          return ImageWidget(imageItems: imageItems);
+                        },
+                      ),
                   ),
-                );
-              })
-
             ],
           ),
         ),
