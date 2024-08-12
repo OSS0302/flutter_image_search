4648471d102983276e5ad4fca7e656333a3413dc
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-08-12 17:29:02 +0900

    07 mvvm + provider + getIt + router + state +result + freezed

--- lib/core/result.dart
+++ lib/core/result.dart
@@ -1,16 +1,9 @@
-sealed class Result<T> {
-  factory Result.success(T data) = Success;
-  factory Result.error(Exception e) = Error;
+import 'package:freezed_annotation/freezed_annotation.dart';
+
+part 'result.freezed.dart';
+
+@freezed
+sealed class Result<T> with _$Result<T> {
+  const factory Result.success(T data) = Success;
+  const factory Result.error(Exception e) = Error;
 }
-
-class Success<T> implements Result<T> {
-  final T data;
-
-  Success(this.data);
-}
-class Error<T> implements Result<T> {
-  Exception e;
-
-  Error(this.e);
-}
-
