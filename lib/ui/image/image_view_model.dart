e64f8fbca2d972f9f720400829d425a9074f0913
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-08-23 14:43:35 +0900

    08 mvvm + provider + getIt + router + state + result + freezed + showSnackBar + showDialog

--- lib/ui/image/image_view_model.dart
+++ lib/ui/image/image_view_model.dart
@@ -1,10 +1,12 @@
 import 'dart:async';
 
 import 'package:flutter/material.dart';
+import 'package:image_search_app/core/result.dart';
 import 'package:image_search_app/ui/image/image_state.dart';
 
 import '../../data/model/image_item.dart';
 import '../../data/repository/image_repository.dart';
+import 'image_event.dart';
 
 class ImageViewModel extends ChangeNotifier {
   final ImageRepository _repository;
@@ -18,18 +20,35 @@ class ImageViewModel extends ChangeNotifier {
 
   ImageState get state => _state;
 
+  final _eventController = StreamController<ImageEvent>();
+
+  Stream<ImageEvent> get eventStream => _eventController.stream;
+
   Future<bool> fetchImage(String query) async {
     _state = state.copyWith(
       isLoading: true,
     );
     try {
       final result = await _repository.getImageItem(query);
-      _state = state.copyWith(
+      switch(result){
 
-        isLoading: false,
-        imageItem: result,
-      );
-      notifyListeners();
+        case Success<List<ImageItem>>():
+          _state = state.copyWith(
+
+            isLoading: false,
+            imageItem: result.data.toList(),
+          );
+          _eventController.add(ImageEvent.showSnackBar('데이터 가져오기 성공'));
+          _eventController.add(ImageEvent.showDialog('다이얼로그 '));
+          notifyListeners();
+
+        case Error<List<ImageItem>>():
+          _state = state.copyWith(
+
+            isLoading: false,
+          );
+          notifyListeners();
+      }
 
       return true;
     }catch(e) {
