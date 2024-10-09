575dafe84dfb8b97465d033e91be82a7db5a7de8
OSS0302 <103983011+OSS0302@users.noreply.github.com>
2024-10-09 16:50:33 +0900

    오류 수정

--- lib/routes.dart
+++ lib/routes.dart
@@ -1,16 +1,27 @@
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
+import 'package:image_search_app/di/di_setup.dart';
 import 'package:image_search_app/presentation/component/gallery/gallery_screen.dart';
 import 'package:image_search_app/presentation/home/home_screen.dart';
 import 'package:image_search_app/presentation/pixabay/pixabay_screen.dart';
+import 'package:image_search_app/presentation/pixabay/pixabay_view_model.dart';
 import 'package:image_search_app/presentation/profile/profile_screen.dart';
 import 'package:image_search_app/presentation/setting/setting_screen.dart';
+import 'package:provider/provider.dart';  // Provider를 사용하기 위해 import 추가
 
 final router = GoRouter(
   routes: [
     GoRoute(
       path: '/',
-      builder: (context, state) => const PixabayScreen(),
+      builder: (context, state) => const HomeScreen(),
+    ),GoRoute(
+      path: '/pixabayScreen',
+      builder: (context, state) {
+        return ChangeNotifierProvider(
+          create: (_) => getIt<PixabayViewModel>(), // 수정된 부분: searchUseCase 대신 getIt 호출
+          child: const PixabayScreen(),
+        );
+      },
     ),
     GoRoute(
       path: '/gallery',
@@ -24,13 +35,5 @@ final router = GoRouter(
       path: '/ProfileScreen',  // 내 프로필 경로
       builder: (context, state) => const ProfileScreen(), // 프로필 화면을 라우트에 추가
     ),
-    // GoRoute(
-    //   path: '/notifications',
-    //   builder: (context, state) => const NotificationsScreen(), // 알림 화면 예시
-    // ),
-    // GoRoute(
-    //   path: '/help',
-    //   builder: (context, state) => const HelpScreen(), // 도움말 화면 예시
-    // ),
   ],
 );
